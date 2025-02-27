dcdec6219794d8cca943d7ac08df5126f2389ce1
shilpi verma <sverma@nobul.com>
2025-02-27 08:36:46 +0530

    autocomplete assignment

--- src/App.js
+++ src/App.js
@@ -1,86 +1,98 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 import { Octokit } from "@octokit/core";
 
-import SearchBar from './Components/search/SearchBar.jsx';
+import SearchBar from "./Components/search/SearchBar.jsx";
 import Cards from "./Components/Cards/Cards.jsx";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      searchQuery:"",
-      results:[],
-      activeSuggestions:"",
-      filteredSuggestions:[],
-      showSuggestions:false,
-      error:null
-    }
+    this.state = {
+      searchQuery: "",
+      results: [],
+      activeSuggestions: "",
+      filteredSuggestions: [],
+      showSuggestions: false,
+      error: null,
+    };
   }
 
-  debounce = (func,delay)=>{
-    console.log("g",delay);
+  debounce = (func, delay) => {
+    console.log("g", delay);
     let timer;
-    return function(){
-      let context= this;
+    return function () {
+      let context = this;
       let args = arguments;
-      const later = function() {
+      const later = function () {
         timer = null;
         func.apply(context, args);
       };
       clearTimeout(timer);
       timer = setTimeout(later, delay);
     };
-    }
+  };
 
-  search = this.debounce(async(query)=>{
-    try{
-      const octokit = new Octokit({ auth: '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' });
-      const response = await octokit.request('GET /search/users', {
-      q: query
-    });
-
+  search = this.debounce(async (query) => {
+    try {
+      const octokit = new Octokit({
+        auth: "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",
+      });
+      const response = await octokit.request("GET /search/users", {
+        q: query,
+      });
       const arrOfNameImageObjects = [];
-      await Promise.all(response.data.items.map(async (item)=> {
-        let newData = await octokit.request(item.url)
-        if(newData){
-          let objNameImage = {};
-          objNameImage['name']=newData.data.name;
-          objNameImage['avatar_url']=newData.data.avatar_url;
-          objNameImage['id']=newData.data.id;
-          arrOfNameImageObjects.push(objNameImage);
-        }
-      }))
-    this.setState({results:arrOfNameImageObjects})
-    }catch(err){
-      this.setState({error:`No results`})
+      return await Promise.all(
+        response.data.items.map(async (item) => {
+          let newData = await octokit.request(item.url);
+          if (newData) {
+            let objNameImage = {};
+            objNameImage["name"] = newData.data.name;
+            objNameImage["avatar_url"] = newData.data.avatar_url;
+            objNameImage["id"] = newData.data.id;
+            arrOfNameImageObjects.push(objNameImage);
+          }
+        })
+      );
+      this.setState({ results: arrOfNameImageObjects });
+    } catch (err) {
+      this.setState({ error: `No results` });
       console.log(`Unable to fetch result ${err.message}`);
     }
-  },300);
+  }, 300);
 
+  handleChange = (e) => {
+    this.setState(
+      {
+        searchQuery: e.target.value,
+      },
+      () => {
+        this.state.searchQuery && this.search(this.state.searchQuery);
+      }
+    );
+  };
 
-  handleChange=(e)=>{
-    this.setState({
-      searchQuery:e.target.value,
-    },()=>{
-      this.state.searchQuery && this.search(this.state.searchQuery)
-    })
-  }
-
-
-  render(){
-    console.log("res",this.state.results)
+  render() {
     const suggestionData = this.state.results;
     return (
       <div className="app">
-        <SearchBar value={this.state.searchQuery} onChange={this.handleChange} />
-        {this.state.error?
+        <SearchBar
+          value={this.state.searchQuery}
+          onChange={this.handleChange}
+        />
+        {this.state.error ? (
           <p>{this.state.error}</p>
-        :
+        ) : (
           <Cards results={suggestionData} />
-        }
+        )}
+        <input type="text" />
+        <ul>
+          <li>Home</li>
+          <li>Rooms</li>
+          <li>bedroom</li>
+        </ul>
       </div>
-    )
+    );
   }
 }
 
