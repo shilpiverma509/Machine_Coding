ef8f4f6441f4d750088bf0ce2a844bc9decf4930
shilpi verma <sverma@nobul.com>
2025-02-27 08:51:40 +0530

    Removing secrets

--- src/Components/useUserSearch.js
+++ src/Components/useUserSearch.js
@@ -1,32 +1,31 @@
-import {useEffect,useState} from "react";
+import { useEffect, useState } from "react";
 import { Octokit } from "@octokit/core";
 
-
-const useUserSearch = (query,pageNumber)=>{
-
-  useEffect(async()=>{
-    const octokit = new Octokit({ auth: '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' });
-      const response = await octokit.request('GET /search/users', {
+const useUserSearch = (query, pageNumber) => {
+  useEffect(async () => {
+    const octokit = new Octokit({ auth: "" });
+    const response = await octokit.request("GET /search/users", {
       q: query,
-      per_page:30,
-      page: pageNumber
+      per_page: 30,
+      page: pageNumber,
     });
     const arrOfNameImageObjects = [];
-      await Promise.all(response.data.items.map(async (item)=> {
-        let newData = await octokit.request(item.url)
-        if(newData){
+    await Promise.all(
+      response.data.items.map(async (item) => {
+        let newData = await octokit.request(item.url);
+        if (newData) {
           let objNameImage = {};
-          objNameImage['name']=newData.data.name;
-          objNameImage['avatar_url']=newData.data.avatar_url;
-          objNameImage['id']=newData.data.id;
+          objNameImage["name"] = newData.data.name;
+          objNameImage["avatar_url"] = newData.data.avatar_url;
+          objNameImage["id"] = newData.data.id;
           arrOfNameImageObjects.push(objNameImage);
         }
-      }))
-      console.log("resukt",arrOfNameImageObjects);
+      })
+    );
+    console.log("resukt", arrOfNameImageObjects);
     // this.setState({results:arrOfNameImageObjects})
-  },[query,pageNumber]
-  )
+  }, [query, pageNumber]);
   return null;
-}
+};
 
 export default useUserSearch;
